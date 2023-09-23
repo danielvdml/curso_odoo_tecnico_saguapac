757d8402d092b8507f9b26239b773482fe337f6a
Daniel Moreno <danielvdmlfiis@gmail.com>
2023-09-23 14:46:44 -0500

    [UPD] Módulo sg_inmobiliario

---
+++ sg_inmobiliario/__manifest__.py
@@ -0,0 +1,18 @@
+{
+    "name":"Seguimiento de inmuebles",
+    "description":"""
+Este módulo permite al corredor administrar inmuebles, y dar seguimientos a los interesados.
+Los tipos de inmuebles que te permitirá administrar son: ....
+    """,
+    "author":"Daniel Moreno <daniel@bigodoo.com>",
+    "depends":[
+        "base"
+    ],
+    "data":[
+        "securitic/res.groups.xml",
+        "securitic/ir.model.access.xml",
+        "demo/inmuebles.xml",
+        "data/sg_tipo_inmueble.xml",
+        "views/sg_inmueble.xml",
+    ]
+}
