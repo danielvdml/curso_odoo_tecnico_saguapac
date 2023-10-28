b580d98b665898e165330dbee60a2798a4fdf4ca
Daniel Moreno <danielvdmlfiis@gmail.com>
2023-10-28 13:47:41 -0500

    [UPD] reporte de impresion

---
+++ sg_sale_order_custom_reports/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    "name":"Reportes en Ventas",
+    "depends":["base","sale"],
+    "data":[
+        "data/paperformat.xml",
+        "reports/external_layout_sg_custom.xml",
+        "reports/report_sale_order.xml"
+    ],
+    "assets":{
+        "web.report_assets_common":[
+            "sg_sale_order_custom_reports/static/src/scss/styles.scss",
+        ]
+    }
+}
