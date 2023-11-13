3a3ba3dc48869be2680c7281fba5e2cdb24f8804
Daniel Moreno <danielvdmlfiis@gmail.com>
2023-11-13 09:37:49 -0500

    [UPD] métodos para uso en json rpc

--- sg_helpdesk_page/models/helpdesk_ticket.py
+++ sg_helpdesk_page/models/helpdesk_ticket.py
@@ -1,4 +1,4 @@
-from odoo import models,fields
+from odoo import models,fields,api
 
 
 class HelpdeskTicket(models.Model):
@@ -7,3 +7,13 @@ class HelpdeskTicket(models.Model):
     ubicacion_agencia_id = fields.Many2one("sg.ubicacion.agencia",string="Ubicación de la agencia   ")
 
 
+    def hace_algo(self,valor):
+        results = []
+        for record in self:
+            results.append(f"{record.name} - {valor}")
+        return results
+
+    @api.model
+    def busqueda_por_cliente(self,keyword):
+        results = self.search_read([("partner_id.name","ilike",f"%{keyword}%")],["id","name","description"])
+        return results
